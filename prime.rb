4051186906aef5f095dd6eebf28bdeb8327fed2e
Sav Younts <savyounts@gmail.com>
2018-08-04 15:06:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,10 @@
 # Add  code here!
 
-
+require "pry"
 def prime?(num)
   array = (2..num-1).to_a
   prime = array.all? do |i|
-    (num % i != 0 && num > 0) || num == 2
+    num % i != 0 
+    binding.pry
 end
 end
